3cdb50389f02e8131cd09162e0115ba2d8c97c46
salimshaikh1100 <50434628+salimshaikh1100@users.noreply.github.com>
2020-10-02 02:24:00 +0530

    Update ReverseString.java

--- Samples/src/main/java/todo/ReverseString.java
+++ Samples/src/main/java/todo/ReverseString.java
@@ -1,5 +1,22 @@
 package main.java.todo;
+package com.compare;
+import java.util.Scanner;
 
 public class ReverseString {
 
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		int n = 0;
+		String rev="";
+		System.out.println("Enter original String: ");
+		String org = sc.nextLine();
+		n=org.length();
+		
+		for(int i=n-1;i>=0;i--)
+		{
+			rev =  rev + org.charAt(i);
+		}
+		System.out.println("Duplicate String: \n"+rev);
+	}
 }
